62b00bf86b63334b057feadec290a11be74c4dc2
SydneyDeb <sydneyd30@nycstudents.net>
2023-10-04 09:31:52 -0400

    added a method

--- src/HairTie.java
+++ src/HairTie.java
@@ -1,2 +1,83 @@
-package PACKAGE_NAME;public class HairTie {
+public class HairTie {
+    private String color;
+    private int diameter;
+    private double price;
+    private boolean isStretchy;
+    final double pi = 3.1415;
+
+    public HairTie(String color, int diameter, double price, boolean isStretchy) {
+        this.color = color;
+        this.diameter = diameter;
+        this.price = price;
+        this.isStretchy = isStretchy;
+    }
+
+    public HairTie() {
+        color = "pink";
+        diameter = 1;
+        price = 0.30;
+        isStretchy = true;
+    }
+
+    // "getter" method that returns the color
+    public String getColor() {
+        return color;
+    }
+
+
+    // "setter" method that sets the color to a new value
+    public void setColor(String newColor) {
+        color = newColor;
+    }
+
+    // "getter" method that returns the diameter value
+    public int getDiameter() {
+        return diameter;
+    }
+
+
+    // "setter" method that sets the diameter value
+    public void setDiameter(int newDiameter) {
+        diameter = newDiameter;
+    }
+
+    // "getter" method that returns the price
+    public double getPrice() {
+        return price;
+    }
+
+
+    // "setter" method that sets the price to a new value
+    public void setPrice(double newPrice) {
+        price = newPrice;
+    }
+
+    // "getter" method that returns if stretchy
+    public boolean getStretchy() {
+        return isStretchy;
+    }
+
+
+    // "setter" method that sets if stretchy boolean to a new value
+    public void setStretchy(boolean newStretchy) {
+        isStretchy = newStretchy;
+    }
+
+    // method that calculates and returns the radius of the hair tie
+    public double calculateRadius() {
+        return (double) diameter/2;
+    }
+
+    // method that calculates and returns the circumference of the hair tie
+    public double calculateCircumference() {
+        return diameter*pi;
+    }
+
+    // method that prints Hair Tie info
+    public void printHairTieInfo() {
+        System.out.println("Color: " + color);
+        System.out.println("Diameter: " + diameter);
+        System.out.println("Price: " + price);
+        System.out.println("Is it stretchy?: " + isStretchy);
+    }
 }
--- src/HairTieRunner.java
+++ src/HairTieRunner.java
@@ -1,2 +1,37 @@
-package PACKAGE_NAME;public class HairTieRunner {
+public class HairTieRunner {
+    public static void main(String [] args) {
+        System.out.println("-- TESTING CONSTRUCTORS AND GETTER METHODS --");
+        HairTie tie1 = new HairTie("blue", 1, 0.20, true);
+        System.out.println(tie1.getColor());
+        System.out.println(tie1.getDiameter());
+        System.out.println(tie1.getPrice());
+        System.out.println(tie1.getStretchy());
+
+        HairTie tie2 = new HairTie();
+        System.out.println(tie2.getColor());
+        System.out.println(tie2.getDiameter());
+        System.out.println(tie2.getPrice());
+        System.out.println(tie2.getStretchy());
+
+        System.out.println("-- TESTING SETTER, CALCULATE RADIUS, CALCULATE CIRCUMFERENCE, AND PRINT HAIR TIE INFO METHODS --");
+        tie1.printHairTieInfo();
+        System.out.println("The radius is: " + tie1.calculateRadius());
+        System.out.println("The circumference is: " + tie1.calculateCircumference());
+        tie1.setColor("red");
+        tie1.setDiameter(2);
+        tie1.setPrice(0.50);
+        tie1.setStretchy(false);
+        System.out.println(tie1.calculateRadius());
+        System.out.println(tie1.calculateCircumference());
+
+        tie2.printHairTieInfo();
+        System.out.println("The radius is: " + tie2.calculateRadius());
+        System.out.println("The circumference is: " + tie2.calculateCircumference());
+        tie2.setColor("yellow");
+        tie2.setDiameter(5);
+        tie2.setPrice(1.20);
+        tie2.setStretchy(false);
+        System.out.println(tie2.calculateRadius());
+        System.out.println(tie2.calculateCircumference());
+    }
 }
